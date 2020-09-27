317299d23b3744ddf11c2ccf63032cd4f9c97f14
Manuel Carvalho <burrowsrd@gmail.com>
2020-09-28 07:53:44 +1000

    String Indexing

---
+++ strings2.py
@@ -0,0 +1,62 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+print(parrot[-1])  #last char
+print(parrot[-14]) #First char
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print()
+
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+
+print()
+
+#parrot = "Norwegian Blue"
+print(parrot[0:6]) # Norweg upto but not including index 6
+
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
+
+print()
+
+print(parrot[10:14])
+print(parrot[10:]) #same
+
+print()
+
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+
+print(parrot[:])
+
+print(parrot[-4:2]) #prints nothing, cannot go backwards
+print(parrot[-4:-2]) #Bl
+print(parrot[-4:12]) #Bl
+
+print(parrot[-14:-8]) #Norweg
