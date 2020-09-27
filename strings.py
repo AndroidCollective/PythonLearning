fc64ca61cce0fb0becfb2539558aa10c7f18e014
Manuel Carvalho <burrowsrd@gmail.com>
2020-09-27 19:35:02 +1000

    Initial Commit

---
+++ strings.py
@@ -0,0 +1,30 @@
+import math
+print('Today is a good day to learn Python')
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " World")
+greeting = "Hello"
+name = "Bruce"
+#name = input("Please enter your name ")
+print(greeting + ' ' + name)
+
+age = 24
+
+
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+#print(name + " is " + age +" years old")
+
+print(age)
+print(type(age))
+
+cn = complex(6,6)
+
+print(cn)
+print(math.pi)
+
